e59e7182a1870b2ef177d307f69c01eb8b67cfc5
vytautasskipitis <87714408+vytautasskipitis@users.noreply.github.com>
2022-05-20 03:04:54 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -17,24 +17,23 @@ def find_maximum_distance(
             distance += 1
 
             if i in cities_with_train_station:
-                print(math.trunc(distance), "km to the station")
                 longest_distance.append(math.trunc(distance))
                 distance = 0
-                two_directions = True
+                if cities_with_train_station[0] == cities_with_train_station[-1]:
+                    two_directions = False
+                else:
+                    two_directions = True
 
         elif two_directions:
             distance += 0.5
 
             if i in cities_with_train_station:
-                print(math.trunc(distance), "km to the station")
                 longest_distance.append(math.trunc(distance))
                 distance = 0
             if cities_with_train_station[-1] == i:
                 two_directions = False
 
-        else:
-            print("error")
-    print(longest_distance)
+        longest_distance.append(math.trunc(distance))
 
     return max(longest_distance)
 
@@ -46,5 +45,6 @@ if __name__ == "__main__":
     assert find_maximum_distance(number_of_cities=5, cities_with_train_station=[0, 4]) == 2
     assert find_maximum_distance(number_of_cities=30, cities_with_train_station=[0, 4, 15, 22, 26]) == 5
     assert find_maximum_distance(number_of_cities=10, cities_with_train_station=[2, 8]) == 3
+    assert find_maximum_distance(number_of_cities=10, cities_with_train_station=[0]) == 9
 
     print("ALL TESTS PASSED")
